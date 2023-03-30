79dde3801e6536aee76bf7460752fa9f0968e9e8
JCYhapon <98503854+JCYhapon@users.noreply.github.com>
2023-03-30 18:17:09 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialExampleModule } from '../material.module';
+import { TabGroupBasicExample } from './tab-group-basic-example';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [TabGroupBasicExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  bootstrap: [TabGroupBasicExample],
+})
+export class AppModule {}
