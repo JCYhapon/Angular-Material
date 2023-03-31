130d9609ef4ae546d74f1e181747d0f6c4c37333
JCYhapon <98503854+JCYhapon@users.noreply.github.com>
2023-03-31 13:37:38 +0800

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialExampleModule } from '../material.module';
 import { TabGroupBasicExample } from './tab-group-basic-example';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoute: Routes = [];
 
 @NgModule({
-  declarations: [TabGroupBasicExample],
+  declarations: [TabGroupBasicExample, NavbarComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
@@ -21,6 +29,11 @@ import { MatButtonModule } from '@angular/material/button';
     MaterialExampleModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRoute),
   ],
   bootstrap: [TabGroupBasicExample],
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {}
